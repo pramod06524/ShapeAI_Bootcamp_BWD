187d02701a6f962588faf7d5a19d04129394984f
pramod06524 <85482139+pramod06524@users.noreply.github.com>
2021-06-07 13:41:16 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+function Footer(){
+var currYear= new Date().getFullYear();
+return(
+  <footer>
+    <p> copyright @ {currYear}</p>
+    </footer>);
+}
+export default Footer;
---
+++ index.js
@@ -0,0 +1,9 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+import App from "./components/App";
+ReactDom.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
